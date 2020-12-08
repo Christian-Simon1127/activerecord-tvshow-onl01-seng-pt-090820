abbee98a70bc7ebcae7849bafa4049f1c3d8590c
Christian Simon <csimon1127@gmail.com>
2020-12-08 02:08:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,11 @@
 class Show < ActiveRecord::Base 
 
-def self.highest_rating 
-  Show.order(rating: :desc).first.rating
-end
+  def self.highest_rating 
+    Show.order(rating: :desc).first.rating
+  end
+  
+  def self.most_popular_show
+    Show.find_by(rating: self.highest_rating)
+  end
   
 end
